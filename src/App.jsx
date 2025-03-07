b468bed1fb64646a2b4252018f057eb3bf46b738
Ansar Sultan <ansarsultan67@gmail.com>
2025-03-07 23:14:22 +0500

    worked on add new proejct button for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: [],
+  });
+
+  function handleStatedAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState=>{
+      const newProject ={
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if(projectsState.selectedProjectID === null){
+    content =  <NewProject onAdd={handleAddProject}/>;
+  }else if(projectsState.selectedProjectID === undefined){
+    content =  <NoProjectSelected onStateAddProject={handleStatedAddProject}/>
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-     <ProjectsSidebar/>
-     {/* <NewProject/> */}
-     <NoProjectSelected/>
+      <ProjectsSidebar onStateAddProject={handleStatedAddProject}/>
+      {content}
     </main>
   );
 }
